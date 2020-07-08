b995e29b2d6f55e3419dcdb3d2f03b8f2c3a8752
joe-ascroft <joeascroftnz1@gmail.com>
2020-07-08 16:14:34 +1200

    gdp data update

---
+++ gdp-cap.R
@@ -0,0 +1,7 @@
+## GDP per capita data from OECD ##
+## load data ##
+
+gdp_cap = read.csv('https://raw.githubusercontent.com/joe-ascroft/Charts2020/master/gdp_cap.csv')
+attach(gdp_cap)
+summary(gdp_cap)
+
